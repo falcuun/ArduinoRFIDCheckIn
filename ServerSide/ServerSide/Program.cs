467198d30b402ad839af2248ce733b2489562568
falcuun <falcuun@gmail.com>
2019-02-27 23:55:24 +0100

    Chosing Port name Added

--- ServerSide/ServerSide/Program.cs
+++ ServerSide/ServerSide/Program.cs
@@ -9,10 +9,20 @@ namespace ServerSide
         {
             SerialPort sp = new SerialPort();
             string[] available_ports = SerialPort.GetPortNames();
+            int index = 0;
+
+            Console.WriteLine("Available Ports: ");
+
             foreach(string port in available_ports) {
+                Console.Write(index + ": ");
                 Console.WriteLine(port);
+                index++;
             }
 
+            Console.Write("Choose The Port By Index: ");
+            int choice = Console.ReadLine()[0] - '0';
+
+            Console.WriteLine("You picked: " + available_ports[choice] + " Port");
             Console.ReadLine();
         }
     }
