7410cdafa9b7719228707968a04ce92aed798a89
falcuun <falcuun@gmail.com>
2019-03-03 21:55:31 +0100

    Added Adafruit's SSD1306 lib for 0.96" 128x64 screens

--- ArduinoRFID/ArduinoRFID.ino
+++ ArduinoRFID/ArduinoRFID.ino
@@ -1,19 +1,29 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define SCREEN_W 128
+#define SCREEN_H 64
 
 #define SS_PIN 10
 #define RST_PIN 9
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+Adafruit_SSD1306 display(SCREEN_W, SCREEN_H, &Wire, -1);
 
 void setup()
 {
     Serial.begin(9600);
     SPI.begin();
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     mfrc522.PCD_Init();
+    display.clearDisplay();
 }
 
 void loop()
 {
+
     if (mfrc522.PICC_IsNewCardPresent())
     {
         byte uid[4];
